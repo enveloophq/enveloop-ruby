e74e8df94ee8c6a209b2a872dc93d76372a01f6e
Ben Wyrosdick <ben@buildplane.com>
2022-10-25 11:55:33 -0500

    allow defaults and send sdk version

--- lib/enveloop/client.rb
+++ lib/enveloop/client.rb
@@ -5,19 +5,20 @@ module Enveloop
       @api_key = api_key
     end
 
-    def send_message(template:, to:, from:, subject:, template_variables: {})
+    def send_message(template:, to:, from: nil, subject: nil, template_variables: {})
       data = {
         to: to,
         from: from,
         subject: subject,
-        templateVariables: template_variables
+        templateVariables: template_variables,
       }
 
       conn = Faraday.new(
         url: @endpoint,
         headers: {
           "Content-Type" => "application/json",
-          "Authorization" => "token #{@api_key}"
+          "Authorization" => "token #{@api_key}",
+          "Sdk-Version" => "ruby-#{Enveloop::VERSION}"
         }
       )
 
